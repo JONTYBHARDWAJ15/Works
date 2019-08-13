00b01fb47688caf98b5a4e2c83041ad052a74ffc
JONTYBHARDWAJ15 <JONTYBHARDWAJ15@users.noreply.github.com>
2019-08-13 09:44:47 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- vigenere.c
+++ vigenere.c
@@ -26,28 +26,27 @@ int main(int argc, char **argv)
     string plaintext = get_string("plaintext: ");
     printf("ciphertext: ");
     
-    for (int i = 0,index = 0,  l = strlen(plaintext); i < l; i++)
+    for (int i = 0, index = 0,  l = strlen(plaintext); i < l; i++)
     {
         if (isalpha(plaintext[i]))
         {
-           // c = (plaintext[i] -'a' + k) % 26 + 'a'
-           if islower(plaintext[i])
-           {
-               printf("%c", (plaintext[i] - 'a' + toupper(k[index]) - 'A') % 26 + 'a');
-            
-           }
-           // c = (plaintext[i] -'A' + k) % 26 + 'A'
-           else if isupper(plaintext[i])
-           {
-               printf("%c", (plaintext[i] - 'A' + toupper(k[index]) - 'A') % 26 + 'A');
-           }
-           index = (index + 1) % lenk; 
+            // c = (plaintext[i] -'a' + k) % 26 + 'a'
+            if islower(plaintext[i])
+            {
+                printf("%c", (plaintext[i] - 'a' + toupper(k[index]) - 'A') % 26 + 'a');
+            }
+            // c = (plaintext[i] -'A' + k) % 26 + 'A'
+            else if isupper(plaintext[i])
+            {
+                printf("%c", (plaintext[i] - 'A' + toupper(k[index]) - 'A') % 26 + 'A');
+            }
+            index = (index + 1) % lenk; 
         }  
         
         else 
-           {
-               printf("%c", plaintext[i]);
-           }    
+        {
+            printf("%c", plaintext[i]);
+        }    
         
     }
     printf("\n");
